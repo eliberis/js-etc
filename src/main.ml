a985c2c248a2e0f361746d635f34729c2f3548f5
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-07-21 11:14:29 -0400

    Basic networking

--- src/main.ml
+++ src/main.ml
@@ -9,12 +9,14 @@ let () =
                 ~doc:"Hostname"
             +> flag "-port" (optional_with_default 25000 int)
                 ~doc:"Port"
-            +> flag "-name" (optional_with_default "TEAM_NAME" string)
+            +> flag "-name" (optional_with_default "COULOMB" string)
                 ~doc:"Team name"
         )
         (fun host port team_name () ->
            printf "Hello from %s at %s:%d\n" team_name host port;
-           return ()
+           Network.loop ~host ~port
+               ~f:(fun ~line ~write -> print_endline line |> return)
+               ~on_connect:(fun write -> write "HELLO COULOMB")
         )
     in
     Command.run command;;
