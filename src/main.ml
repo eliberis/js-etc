d2562341b625f85043d6abcb3905244424e31f48
Edgar Liberis <edgaras.liberis@gmail.com>
2016-07-21 10:27:39 -0400

    Nicer main

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,20 @@
-open Core.Std;;
+open Async.Std;;
 
 let () =
-  printf "Hello, world!"
-;;
+    let command = Command.async
+        ~summary: "etc trading program"
+        Command.Spec.(
+            empty
+            +> flag "-host" (optional_with_default "localhost" string)
+                ~doc:"Hostname"
+            +> flag "-port" (optional_with_default 25000 int)
+                ~doc:"Port"
+            +> flag "-name" (optional_with_default "TEAM_NAME" string)
+                ~doc:"Team name"
+        )
+        (fun host port team_name () ->
+           printf "Hello from %s at %s:%d\n" team_name host port;
+           return ()
+        )
+    in
+    Command.run command;;
