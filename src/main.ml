634a918483565305d83ea56d246a5eea2ae10391
Edgar Liberis <edgaras.liberis@gmail.com>
2016-07-22 14:01:35 -0400

    margin

--- src/main.ml
+++ src/main.ml
@@ -24,16 +24,16 @@ let () =
                             match msg with
                             | Fill _ -> print_endline (Message.Server.sexp_of_t msg |> Sexp.to_string) |> return
                             | _ -> return ())
-                        (* ; Predicted.penny ~symbol:Symbol.AMZN ~margin:1 *)
+                        ; Predicted.penny ~symbol:Symbol.AMZN ~margin:1
                         ; Predicted.penny ~symbol:Symbol.DIS ~margin:1
-                        (*; Predicted.penny ~symbol:Symbol.DUK ~margin:1
+                        ; Predicted.penny ~symbol:Symbol.DUK ~margin:1
                         ; Predicted.penny ~symbol:Symbol.HD ~margin:1
                         ; Predicted.penny ~symbol:Symbol.KO ~margin:1
                         ; Predicted.penny ~symbol:Symbol.NEE ~margin:1
                         ; Predicted.penny ~symbol:Symbol.PG ~margin:1
                         ; Predicted.penny ~symbol:Symbol.PM ~margin:1
-                        ; Predicted.penny ~symbol:Symbol.RSP ~margin:1
-                        ; Predicted.penny ~symbol:Symbol.SO ~margin:1 *)
+                        (* ; Predicted.penny ~symbol:Symbol.RSP ~margin:1 *)
+                        ; Predicted.penny ~symbol:Symbol.SO ~margin:1
                         (* ; Predicted.penny ~symbol:Symbol.XLP ~margin:3 *)
                         (*; Predicted.penny ~symbol:Symbol.XLU ~margin:1
                         ; Predicted.penny ~symbol:Symbol.XLY ~margin:1*)
